332080c7b3adca41f770cab35f3df1f91c29ab38
sahasuresh <73297561+sahasuresh@users.noreply.github.com>
2020-12-05 22:02:28 +0530

    message pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello to World! \n --From Suresh'));
+app.get('/', (req, res) => res.send('Hello to World! <br> --From Suresh'));
+app.get('/', (req, res) => res.send('Completing pipeline test'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
